4705dee64629dfa8dc52cb3bfeb0442c7fc54ddd
Abdurrahmon2700 <sabrin1998@inbox.ru>
2022-04-19 14:58:11 +0500

    responsive

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ let newSelect = document.querySelector("select");
 let elconfetti = document.querySelector("#my-canvas")
 let elOutput = document.querySelector("#output");
 let elModal = document.querySelector("#modal");
+let elcontainer = document.querySelector(".container")
+let closeBtn = document.querySelector("#close")
+
+closeBtn.addEventListener("click", (e) => {
+  elModal.style.display = "none";
+  elconfetti.style.display = "none"
+})
+
 
 let nameOfUnevercity = ["O’zbekiston milliy universiteti", "Toshkent davlat texnika universiteti","Toshkent davlat iqtisodiyot universiteti", "O’zbekiston davlat jahon tillari universiteti", "Toshkent davlat sharqshunoslik instituti", "Toshkent arxitektura-qurilish instituti", "Toshkent to’qimachilik va engil sanoat istituti", "Toshkent avtomobil-yo’llari instituti"];
 
@@ -20,7 +28,7 @@ for (let univer = 0; univer < nameOfUnevercity.length; univer++){
 
 
 elBtn.addEventListener('click', (e) => {
- 
+  e.preventDefault(         )
   let userBall = userInput.value;
   userBall = Number(userBall)
   let univeName = newSelect.value;
@@ -29,18 +37,24 @@ elBtn.addEventListener('click', (e) => {
     userInput.value = "";
     return
   }
+
+  else if (userInput.value = ""){
+    userInput.style.borderColor = "red";
+    return;
+  }
+
   else if (userBall < enterPoint) {
     elModal.style.display = "block"
     elOutput.textContent =`Afsus ${userBall} ball bilan ${univeName} ga kira olmadingiz. Keyingi yil harakat qilib ko'ring.`
     selectWraper.addEventListener('click' ,(e) => {
     })
-
-    
   }
+
   else if (userBall >= enterPoint && userBall <= contract) {
     elOutput.textContent =` ${userBall} ball bilan ${univeName} ga Kontrakt asosida o'qishga qabul qilindizgiz.`
     elModal.style.display = "block"
   }
+
   else if (userBall > contract) {
     elModal.style.display = "block"
     elOutput.textContent =`Tabriklaymiz ${userBall} ball bilan ${univeName} ga Grand asosida o'qishga qabul qilindizngiz`
@@ -49,11 +63,11 @@ elBtn.addEventListener('click', (e) => {
     confetti.render();
     elconfetti.style.display = "block"
   }
+
   elconfetti.addEventListener('click', (e) => {
       elconfetti.style.display = "none"
       elModal.style.display = "none"
       userInput.value = ""
   })
-
 })
 
